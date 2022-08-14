e0a5759e2590f8b0ad90e91d82de65e980c7b22d
Noah Hoffman <stacknoah99@gmail.com>
2022-08-14 12:21:02 -0600

    changed info area to a better styling format

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -7,7 +7,7 @@ import "swiper/css";
 // import "swiper/css/pagination";
 import "swiper/css/navigation";
 
-import img1 from '../images/housePic.jpg'
+import img1 from '../images/lumberSky.jpg'
 import img2 from '../images/commericalRoof.jpg'
 import img3 from '../images/lumberLabor.jpg'
 import img4 from '../images/manOnRoof.jpg'
@@ -29,7 +29,7 @@ function Carousel() {
       >
         <SwiperSlide className="swiper-slide">
           <img src={img1} alt={img1} />
-          <h1>Residentinal</h1>
+          
         </SwiperSlide>
         <SwiperSlide className="swiper-slide"><img src={img2} alt={img2} /></SwiperSlide>
         <SwiperSlide className="swiper-slide"><img src={img3} alt={img3} /></SwiperSlide>
--- src/components/textInfo.js
+++ src/components/textInfo.js
@@ -1,48 +1,20 @@
 import React from 'react'
-import roofPicture from '../images/roofing.jpg'
-import skylightPicture from '../images/skylight.jpg'
-import solarPanel from '../images/solarPanel.jpg'
 
 function textInfo() {
     return (
-        <div>
-            <article className="roofblock">
-                <div className="rooftext">
-                    <h3> <strong>Roof Replacement</strong></h3>
-                    <p>Looking for a reliable contractor with extensive experience for your next
-                        project? At Colorado Building Solutions, we stand by the excellence of our work and provide clients with
-                        personalized attention based on their specific needs. If you’re looking for professional Roof
-                        Replacement services, please get in touch for your consultation.</p>
-                </div>
-                <div>
-                    <img className="roofImage" src={roofPicture} alt=""/>
-                </div>
-            </article>
-            <article className="skylightblock">
-                <div>
-                    <img className="skylightImage" src={skylightPicture} alt=""/>
-                </div>
-                <div className="skylightText">
-                    <h3>SkyLight Installation </h3>
-                    <p> At Colorado Building Solutions, our clients are our number one priority and we
-                        go the extra mile to make sure they’re completely satisfied. With this service, we’re fully prepared to
-                        tackle even the most complex projects and stand by the exceptional quality of our work. Call us today
-                        and find out more about what we can do for you.</p>
-                </div>
-            </article>
-            <article className="solarblock">
-                <div className="solarText">
-                    <h3>Solar Panel Installation</h3>
-                    <p>When you’re looking for top quality work, Colorado Building Solutions is here
-                        to help. Our contracting professionals are available to provide a wide range of customizable options,
-                        all guaranteed to meet and exceed expectations. Contact our office today to learn more about this and
-                        our other available services.</p>
-                </div>
-                <div>
-                    <img className="solarImage" src={solarPanel} alt="" />
-                </div>
-            </article>
-        </div>
+        <main>
+            <div class='banner'></div>
+            <p>Candy sesame snaps bear claw cookie. Tart marzipan dessert gummies. Marzipan dragée wafer croissant cake. Biscuit muffin carrot cake.</p>
+            <p>Fruitcake donut icing gummi bears. Donut cake jujubes wafer chocolate bar cotton candy carrot cake. Bear claw tart sweet powder cake. Gummies ice cream dessert cupcake pudding jelly-o.</p>
+            <div class='banner'></div>
+            <p>Marshmallow cupcake marzipan topping tootsie roll jelly-o cake liquorice. Toffee cotton candy danish tiramisu bonbon caramels liquorice halvah. Biscuit donut bear claw chupa chups pudding biscuit powder sweet roll donut. Tart candy canes marzipan.</p>
+            <p>Sugar plum pastry bonbon danish apple pie. Cookie cheesecake candy canes chocolate powder dessert fruitcake jelly. Powder halvah sweet sugar plum lollipop.</p>
+            <p>Powder bonbon chocolate caramels cheesecake soufflé. Cake icing icing ice cream pudding jelly carrot cake ice cream tiramisu. Candy canes wafer marzipan pie chocolate bar dessert jujubes. Biscuit oat cake donut icing brownie.</p>
+            <div class='banner'></div>
+            <p>Dragée marzipan lollipop sesame snaps chocolate bar toffee jelly. Croissant cupcake sesame snaps tiramisu gummi bears ice cream sugar plum cotton candy. Soufflé cake gummi bears muffin soufflé soufflé marshmallow macaroon. Caramels caramels lemon drops.</p>
+            <p>Croissant oat cake cake. Cheesecake liquorice muffin chocolate cake candy canes powder muffin tiramisu. Candy canes donut jelly beans jelly beans cotton candy jelly beans muffin. Candy apple pie dessert soufflé tiramisu.</p>
+            
+        </main>
     )
 }
 
