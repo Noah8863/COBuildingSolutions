661eecde4d87d344a7a3894ef27533630012d40d
Noah Hoffman <stacknoah99@gmail.com>
2022-08-16 12:39:24 -0600

    finished landing page and designed contact buttons

--- src/components/CommericalPage.js
+++ src/components/CommericalPage.js
@@ -3,7 +3,10 @@ import { Link } from 'react-router-dom';
 
 function Commerical() {
     return (
-        <h1>Hello</h1>
+        <main>
+            <h1>Commerical roofing service</h1>
+            <p>At Colorado Building Solutions, we provide qulity commerical roofing services. Our past jobs have been  </p>
+        </main>
     )
 }
 export default Commerical;
--- src/components/textInfo.js
+++ src/components/textInfo.js
@@ -5,8 +5,13 @@ function textInfo() {
     return (
         <main className='infoArea'>
             <h2 className='infoAreaTitles'>WHO WE ARE</h2>
-            <p className='infoText'>Colorado Building Solutions serves as a leading contractor in the greater Denver and surrounding areas. We’re a team of fully-certified professionals who tackle everything from complex large projects to smaller scale jobs.Fueled by our commitment to excellence, we go the extra mile to make sure clients are completely satisfied with our work. Call us today to schedule a consultation. </p>
-            <Link to='/quote'><button id='contactBtn'>Contact Us</button></Link>
+            <p className='infoText'>Colorado Building Solutions serves as a leading commercial and residential roofing company, along with a commercial lumber division in the greater Denver and surrounding areas. We’re a team of fully-certified professionals who tackle everything from complex large projects to smaller scale jobs.Fueled by our commitment to excellence, we go the extra mile to make sure clients are completely satisfied with our work. Call us today to schedule a consultation. </p>
+            <Link to='/quote'><button class="learn-more">
+                <span class="circle" aria-hidden="true">
+                    <span class="icon arrow"></span>
+                </span>
+                <span class="button-text">Contact Us</span>
+            </button></Link>
             <div class='banner'></div>
             <h2 className='infoAreaTitles'>Roof Replacement</h2>
             <p className='infoText'>Looking for a reliable contractor with extensive experience for your next project? At Colorado Building Solutions, we stand by the excellence of our work and provide clients with personalized attention based on their specific needs. If you’re looking for professional Roof Replacement services, please get in touch for your consultation.</p>
@@ -17,7 +22,7 @@ function textInfo() {
             <h2 className='infoAreaTitles'>About Us</h2>
             <p className='infoText'>Colorado Building Solutions was founded back in 2020 and is based out of Englewood Colorado. We have partnerships with Nova Incorporations and take pride in all of our services we provide.</p>
         </main>
-        
+
     )
 }
 
