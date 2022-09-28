cf98ac5fcfb3d38b32604ad1b718fcf1ed9632d1
Noah Hoffman <stacknoah99@gmail.com>
2022-09-28 13:41:25 -0600

    changing logo size and making it a PNG

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../images/logo.jpg';
+import logo from '../images/logo.png';
 import { Link } from 'react-router-dom';
 
 export default function NavBar() {
