73febf43da1c3f1d97f4905e9d4d63173eab68ee
Noah8863 <stacknoah99@gmail.com>
2022-11-08 17:27:09 -0700

    cleaning up some old components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './App.scss';
 import NavBar from './components/NavBar/index'
 import Quote from './components/Contact/index'
-import Lumber from './components/Lumber/lumber'
 import Info from './components/InfoContainers/info'
 import Footer from './components/Footer/index'
-import Commerical from './components/CommericalPage/commerical'
-import Residential from './components/ResidentialPage/residential'
 import ContactContainer from './components/ContactContainer/index'
-import './App.scss';
 import Certificate from './components/Certification';
 
 function App() {
